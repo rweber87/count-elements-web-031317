07461cc535ec444967fe1aa04552d537c2e54840
Rob Weber <rob.weber87@gmail.com>
2017-04-03 08:19:38 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.each do |ele|
+  	if !new_hash[ele]
+  		new_hash[ele] = 1
+  	else
+  		new_hash[ele] += 1
+  	end
+  end
+  new_hash
 end
  
